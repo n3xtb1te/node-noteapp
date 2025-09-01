9e45fbc6bef8d3bf9541e45eb783f785187e4567
n3xtb1te <168577163+n3xtb1te@users.noreply.github.com>
2025-09-01 16:59:40 +0200

    feat: add async / await

--- src/notes.ts
+++ src/notes.ts
@@ -2,19 +2,19 @@ import { INFO, REGULAR, WARNING } from '@utils/constants.ts';
 import { loadNotes, saveNotes } from '@utils/helper.ts';
 import type { Note } from '@utils/types.ts';
 
-export const addNote = (note: Note) => {
-  const notes = loadNotes();
+export const addNote = async (note: Note) => {
+  const notes = await loadNotes();
   notes.push(note);
   saveNotes(notes);
 };
 
-export const listNotes = () => {
-  const notes = loadNotes();
+export const listNotes = async () => {
+  const notes = await loadNotes();
   notes.forEach((n) => console.log(REGULAR(` • ${n.title}`)));
 };
 
-export const readNote = (title: string) => {
-  const notes = loadNotes();
+export const readNote = async (title: string) => {
+  const notes = await loadNotes();
   const note = notes.find((item) => item.title === title);
   if (note) {
     console.log(
@@ -25,11 +25,11 @@ export const readNote = (title: string) => {
   }
 };
 
-export const removeNote = (title: string) => {
-  const notes = loadNotes();
+export const removeNote = async (title: string) => {
+  const notes = await loadNotes();
   const newNotes = notes.filter((n) => n.title !== title);
   if (newNotes.length !== notes.length) {
-    saveNotes(newNotes);
+    await saveNotes(newNotes);
     console.log(INFO('Note removed.'));
   } else {
     console.log(WARNING('No such note.'));
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,4 +1,5 @@
-import { existsSync, readFileSync, writeFileSync } from 'node:fs';
+import { constants } from 'node:fs';
+import { access, readFile, writeFile } from 'node:fs/promises';
 import { ERROR, PATH, WARNING } from '@utils/constants.ts';
 import type { Note } from '@utils/types.ts';
 import { v7 as uuidv7 } from 'uuid';
@@ -15,21 +16,29 @@ export const prepareData = (
   };
 };
 
-export const loadNotes = () => {
-  if (!existsSync(PATH)) {
+export const loadNotes = async () => {
+  if (!fileExists(PATH)) {
     console.log(WARNING('No such file.'));
     return [];
   }
   try {
-    return JSON.parse(readFileSync(PATH, 'utf-8')) as Note[];
+    return JSON.parse(await readFile(PATH, 'utf-8')) as Note[];
   } catch {
     console.log(ERROR('Notes file is broken.'));
     return [];
   }
 };
 
-export const saveNotes = (notes: Note[]) => {
-  writeFileSync(PATH, JSON.stringify(notes, null, 2));
+export const saveNotes = (notes: Note[]) =>
+  writeFile(PATH, JSON.stringify(notes, null, 2));
+
+export const fileExists = async (path: string) => {
+  try {
+    await access(path, constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
 };
 
 export const generateUUID = () => uuidv7();
